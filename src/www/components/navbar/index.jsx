1ea3f78d2a5c65d473c8726eddef99a39cb41d15
khangdthe141099 <damtuankhangctz@gmail.com>
2022-04-21 18:07:07 +0700

    connect wallet

--- src/www/components/navbar/index.jsx
+++ src/www/components/navbar/index.jsx
@@ -1,54 +1,106 @@
-import React from 'react';
-import { Search,
-        AccountCircleOutlined,
-        AccountBalanceWalletOutlined
+import React, { useState } from 'react';
+import { idlFactory } from '../../../declarations/superheroes.did.js';
+import {
+	Search,
+	AccountCircleOutlined,
+	AccountBalanceWalletOutlined,
 } from '@mui/icons-material';
 import {
-  Container,
-  Logo,
-  LogoWrapper,
-  Menu,
-  MenuItem,
-  Name,
-  Option,
-  OptionItem,
-  OptionWrapper,
-  Right,
-  SearchInput,
-  SearchWrapper
-} from './navbar-elements'
+	Container,
+	Logo,
+	LogoWrapper,
+	Menu,
+	MenuItem,
+	Name,
+	Option,
+	OptionItem,
+	OptionWrapper,
+	Right,
+	SearchInput,
+	SearchWrapper,
+	WalletAddress,
+	Balance,
+	BalanceNumber,
+	IcpLogo,
+} from './navbar-elements';
 
 function Navbar() {
+	const [prinpId, setPrinpId] = useState();
+
+	const walletAddress = prinpId?.toString();
+
+	const onConnectWallet = async () => {
+		try {
+			const publicKey = await window.ic.plug.requestConnect({
+				whitelist: [process.env.SUPERHEROES_CANISTER_ID],
+			});
+			const NNSUiActor = await window.ic.plug.createActor({
+				canisterId: process.env.SUPERHEROES_CANISTER_ID,
+				interfaceFactory: idlFactory,
+			});
+
+			const princi = await window.ic.plug.agent.getPrincipal();
+			setPrinpId(princi);
+
+			console.log(`The connected user's public key is:`, publicKey);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
 	return (
 		<Container>
 			<LogoWrapper>
-        <Logo src='https://motoko-lsp-client.gallerycdn.vsassets.io/extensions/motoko-lsp-client/motoko-lsp-client/2.2.0/1583876431744/Microsoft.VisualStudio.Services.Icons.Default' alt=''/>
-        <Name>NFPTU</Name>
-      </LogoWrapper>
+				<Logo
+					src='https://motoko-lsp-client.gallerycdn.vsassets.io/extensions/motoko-lsp-client/motoko-lsp-client/2.2.0/1583876431744/Microsoft.VisualStudio.Services.Icons.Default'
+					alt=''
+				/>
+				<Name>NFPTU</Name>
+			</LogoWrapper>
 
 			<Right>
-        <SearchWrapper>
-          <Search style={{color: '#gray'}}/>
-          <SearchInput placeholder="Search items, collections, and accounts"/>
-        </SearchWrapper>
-  
-        <OptionWrapper>
-          <Menu>
-            <MenuItem>Home</MenuItem>
-            <MenuItem>Stats</MenuItem>
-            <MenuItem>Create</MenuItem>
-          </Menu>
+				<SearchWrapper>
+					<Search style={{ color: '#gray' }} />
+					<SearchInput placeholder='Search items, collections, and accounts' />
+				</SearchWrapper>
 
-          <Option>
-            <OptionItem>
-                <AccountCircleOutlined fontSize="large"/>
-            </OptionItem>
-            <OptionItem>
-                <AccountBalanceWalletOutlined fontSize="large"/>
-            </OptionItem>
-          </Option>
-        </OptionWrapper>
-      </Right>
+				<OptionWrapper>
+					<Menu>
+						<MenuItem>Home</MenuItem>
+						<MenuItem>Stats</MenuItem>
+						<MenuItem>Create</MenuItem>
+					</Menu>
+
+					<Option>
+						<OptionItem>
+							<AccountCircleOutlined fontSize='large' />
+						</OptionItem>
+						{!prinpId ? (
+							<OptionItem onClick={onConnectWallet}>
+								<AccountBalanceWalletOutlined fontSize='large' />
+							</OptionItem>
+						) : (
+							<>
+								<OptionItem>
+									<WalletAddress>
+										{walletAddress.slice(0, 3)} ... {walletAddress.slice(60, 63)}
+									</WalletAddress>
+								</OptionItem>
+
+								<OptionItem>
+									<Balance>
+										<BalanceNumber>0</BalanceNumber>
+										<IcpLogo
+											src='https://cryptologos.cc/logos/internet-computer-icp-logo.png'
+											alt=''
+										/>
+									</Balance>
+								</OptionItem>
+							</>
+						)}
+					</Option>
+				</OptionWrapper>
+			</Right>
 		</Container>
 	);
 }
