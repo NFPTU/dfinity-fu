03ac1c2efea00b7b641e374c03d4cfab1b7f7c2d
thiennv <thiennv@sotanext>
2022-05-22 14:15:41 +0700

    fix

---
+++ src/www/pages/admin/nft.js
@@ -0,0 +1,58 @@
+export const metadata = [
+	{
+		token: 'ant-queen',
+		metadata: {
+			name: 'Ant Queen',
+			description: 'Ant Queen',
+			image:
+				'https://storageapi.fleek.co/c86ea07c-070d-40b0-bb7a-12a2d3c468f4-bucket/nft/ant-queen.png',
+		},
+	},
+	{
+		token: 'ant-worker',
+		metadata: {
+			name: 'Ant Worker',
+			description: 'Ant Worker',
+			image:
+				'https://storageapi.fleek.co/c86ea07c-070d-40b0-bb7a-12a2d3c468f4-bucket/nft/ant.png',
+		},
+	},
+	{
+		token: 'ant-nest',
+		metadata: {
+			name: 'Ant Nest',
+			description: 'Ant Nest',
+			image:
+				'https://storageapi.fleek.co/c86ea07c-070d-40b0-bb7a-12a2d3c468f4-bucket/nft/ant.png',
+		},
+		attributes: [
+			{
+				trait_type: 'Type',
+				value: 'LandCore',
+			},
+			{
+				trait_type: 'ID',
+				value: 24780,
+			},
+			{
+				trait_type: 'Rarity',
+				value: 'Uncommon',
+			},
+			{
+				trait_type: 'Level',
+				value: 1,
+				max: 10,
+			},
+			{
+				trait_type: 'Health',
+				value: 1600,
+				max: 4600,
+			},
+			{
+				trait_type: 'Limit',
+				value: 30,
+				max: 92,
+			},
+		],
+	},
+];
