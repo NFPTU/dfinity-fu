4c4f1ee7db85c9373be9c2cad18e683ef17f2f13
anhth2912 <anhth291220@gmail.com>
2022-08-23 00:53:55 +0700

    condition in land

--- src/www/pages/game/nest/index.jsx
+++ src/www/pages/game/nest/index.jsx
@@ -161,6 +161,21 @@ function Nest(props) {
 		}
 	};
 
+	const handleAddQueen = () => {
+		const queenIn = cardSelected?.detail?.nest?.queenIn[0];
+		const inLand = cardSelected?.detail?.nest?.inLand[0];
+		console.log(cardSelected);
+		if(queenIn === 0 || !queenIn) {
+			setOpen(true)
+		}else if(inLand === 0 || !inLand){
+			toast.warn('Nest must add to Land can add Queen');
+			
+		} else {
+			toast.warn('Nest only contain 1 queen');
+		}
+		
+	}
+
 	useEffect(() => {
 		if (principal && superheroes) {
 			onGetData();
@@ -259,7 +274,7 @@ function Nest(props) {
 
 						<BtnList>
 							<Btn onClick={confirmDialog}>Upgrade</Btn>
-							<Btn onClick={() => setOpen(true)}>Add Queen</Btn>
+							<Btn onClick={handleAddQueen}>Add Queen</Btn>
 						</BtnList>
 					</Right>
 				</Wrapper>
